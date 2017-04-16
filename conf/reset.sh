d903c296d170b7739a52e3f24b20360a1ebe46e4
MuLuu09 <muluu09@gmail.com>
2017-04-16 13:42:23 +0800

    Add files via upload

---
+++ conf/reset.sh
@@ -0,0 +1,13 @@
+service ssh restart
+service openvpn restart
+service dropbear restart
+service nginx restart
+service php5-fpm restart
+service webmin restart
+service squid3 restart
+service fail2ban restart
+
+
+echo "OK SUDAH SIAP RESET VPS BIARKAN SAMPAI TEKELUAR"
+echo "SYSTEM BY MuLuu09 TELEGRAM @MuLuu09"
+reboot
