25ca39ff3c2df423759c4a73e28b70414e2fe4b6
MuLuu09 <muluu09@gmail.com>
2017-04-23 03:19:47 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -82,7 +82,7 @@ wget -O /etc/iptables.up.rules "http://raw.github.com/MuLuu09/conf/master/iptabl
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
-sed -i 's/port 1194/port 6500/g' /etc/openvpn/59999.conf
+sed -i 's/port 59999/port 6500/g' /etc/openvpn/59999.conf
 sed -i $MYIP2 /etc/iptables.up.rules;
 iptables-restore < /etc/iptables.up.rules
 service openvpn restart
