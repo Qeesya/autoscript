a08c64dbf2c00ac2bca8ce230f058516d2e54985
MuLuu09 <muluu09@gmail.com>
2017-05-21 11:06:02 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -135,7 +135,7 @@ service fail2ban restart
 apt-get -y install squid3
 wget -O /etc/squid3/squid.conf "http://raw.github.com/MuLuu09/conf/master/squid.conf"
 sed -i $MYIP2 /etc/squid3/squid.conf;
-service squid3 restart
+
 
 # install webmin
 cd
