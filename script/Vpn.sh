cc05898e863dd19bf2ca13ff0108f615be33a49f
MuLuu09 <muluu09@gmail.com>
2017-05-21 10:02:25 +0800

    Update Vpn.sh

--- script/Vpn.sh
+++ script/Vpn.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-# go to root
-cd
-# install openvpn
+#installing normal openvpn, easy rsa & openvpn-nl
+apt-get install easy-rsa -y
+apt-get install openvpn -y
+
+#ipforward
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+iptables -F
+iptables -t nat -F
+iptables -t nat -A POSTROUTING -s 10.8.0.0/16 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.16.0.0/16 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.1.0.0/16 -o eth0 -j MASQUERADE
+iptables-save
+#fast setup with old keys, optional if we want new key
+cd /
 wget -O /etc/openvpn/openvpn.tar "http://raw.github.com/Qeesya/autoscript/master/script/openvpn.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
 wget -O /etc/openvpn/1194.conf "http://raw.github.com/Qeesya/autoscript/master/script/1194.conf"
 service openvpn restart
-sysctl -w net.ipv4.ip_forward=1
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.up.rules "http://raw.github.com/MuLuu09/conf/master/iptables.up.rules"
-sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-sed -i 's/port 1194/port 6500/g' /etc/openvpn/1194.conf
-sed -i $MYIP2 /etc/iptables.up.rules;
-iptables-restore < /etc/iptables.up.rules
+openvpn --remote CLIENT_IP --dev tun0 --ifconfig 10.9.8.1 10.9.8.2
+#get ip address
+apt-get -y install aptitude curl
+
+if [ "$IP" = "" ]; then
+        IP=$(curl -s ifconfig.me)
+fi
+
+# configure openvpn client config
+cd /etc/openvpn/
+wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/Qeesya/autoscript/master/conf/1194-client.conf"
+sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
+PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
+useradd -M -s /bin/false MuLuu
+echo "MuLuu:MuLuu" | chpasswd
+
 service openvpn restart
-rm Vpn.sh
